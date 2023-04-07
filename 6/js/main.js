46876f73d245d4e816dd9d4b61e0190fc172e499
Keks <keks@htmlacademy.ru>
2023-04-08 02:29:42 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,9 @@
+import {getPictures} from './data.js';
+import './photos.js';
+
+
+const photosArray = getPictures();
+
+
+/* eslint-disable */
+console.log(photosArray);
---
+++ 6/js/util.js
@@ -0,0 +1,20 @@
+function getRandomInteger(a, b) {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+const getRandomArrayElement = (array) =>
+  array[getRandomInteger(0, array.length - 1)];
+
+const createIdgenerator = () => {
+  let lastgenerateId = 0;
+
+  return () => {
+    lastgenerateId += 1;
+    return lastgenerateId;
+  };
+};
+
+export {getRandomInteger, getRandomArrayElement, createIdgenerator};
